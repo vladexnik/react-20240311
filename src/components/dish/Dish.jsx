e6954332da22120eafba1fd834d9ed64d36dac24
vladexnik <nikityuk_2015_smile@mail.ru>
2024-04-01 19:18:11 +0300

    add custom hook useCount with useCallback for incr,decr,count

---
+++ src/components/dish/Dish.jsx
@@ -0,0 +1,25 @@
+import useCount from "../hooks/calc-countDish";
+
+const Dish = ({dish}) => {
+      
+    const {name, ingredients, price}=dish;
+    const {count, increment, decrement}=useCount();
+    
+    return (
+        <>  
+            <div>
+                <button onClick={decrement} disabled={count === 0}>-</button>
+                <span>{count}</span>
+                <button onClick={increment} disabled={count === 5}>+</button>
+            </div>
+            <p>{name}: {price}$</p>
+            <ul>{ingredients.map(ingredient=>(
+                    <li key={ingredient}>{ingredient}</li>
+            ))}
+            </ul>
+            <hr/>
+        </>
+    )
+}
+
+export default Dish;
